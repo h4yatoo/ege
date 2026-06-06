e0585e8a4a116317c1b17d0956df102a14aa65fe
h4yatoo <balandin27072008@gmail.com>
2026-06-06 15:58:34 +0200

    homework

--- task - 24/type - 1/replace_type.py
+++ task - 24/type - 1/replace_type.py
@@ -10,22 +10,64 @@
 #24.01
 
 # способ 1
-with open('../files/2401.txt') as f:
-    s = f.readline()
-    s=s.replace('ad', 'a d').replace('da', 'd a')
-    print(max(len(x) for x in s.split()))
+# with open('../files/2401.txt') as f:
+#     s = f.readline()
+#     s=s.replace('ad', 'a d').replace('da', 'd a')
+#     print(max(len(x) for x in s.split()))
 
-# способ 2
-with open('../files/2401.txt') as f:
+# # способ 2
+# with open('../files/2401.txt') as f:
+#     s = f.readline()
+# cur=1
+# max_len=0
+# for i in range(len(s)-1):
+#     if s[i]+s[i+1] in('ad','da'):
+#         cur=1
+#     else:
+#         cur+=1
+#     max_len=max(max_len,cur)
+# print(max_len)
+
+
+
+#1 REPLACE dz
+with open('../files/1873.txt') as f:
+    s=f.readline()
+s=s.replace('PR','P R')
+s=s.replace('RP','R P')
+print(max(len(x) for x in s.split()))
+
+#COUNTER
+with open('../files/1873.txt') as f:
     s = f.readline()
-cur=1
-max_len=0
-for i in range(len(s)-1):
-    if s[i]+s[i+1] in('ad','da'):
-        cur=1
+cur = 1
+max_len = 1
+for i in range(len(s) - 1):
+    if s[i] + s[i + 1] in ('RP', 'PR'):
+        cur = 1
     else:
-        cur+=1
-    max_len=max(max_len,cur)
+        cur += 1
+    max_len = max(max_len, cur)
+print(max_len)
+
+
+#2 REPLACE
+with open('../files/2410.txt') as f:
+    s = f.readline()
+s=s.replace('00', '0 0')
+print(max(len(x) for x in s.split()))
+
+#COUNTER
+with open('../files/2410.txt') as f:
+    s = f.readline()
+cur = 1
+max_len = 1
+for i in range(len(s) - 1):
+    if s[i] + s[i + 1] == '00':
+        cur = 1
+    else:
+        cur += 1
+    max_len = max(max_len, cur)
 print(max_len)
 
 
